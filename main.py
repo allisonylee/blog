99b385d17f279bc589e717c4dbd1f73e5b0ab546
Allison Lee <88457470+allisonylee@users.noreply.github.com>
2026-07-26 13:06:40 -0400

    added authentication

--- main.py
+++ main.py
@@ -110,6 +110,22 @@ async def general_http_exception_handler(request: Request, exception: StarletteH
         status_code=exception.status_code,
     )
 
+@app.get("/login", include_in_schema=False)
+async def login_page(request: Request):
+    return templates.TemplateResponse(
+        request, 
+        "login.html",
+        {"title": "Login"}
+    )
+
+@app.get("/register", include_in_schema=False)
+async def register_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "register.html",
+        {"title": "Register"}
+    )
+
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exception: RequestValidationError):
